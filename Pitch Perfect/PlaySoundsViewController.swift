e2f90b21f811bab5b3360e0f7ab944739b0b972b
Michael Brown <mbrown@incipire.com>
2016-02-05 19:42:11 -0600

    Added chipmunk and vader voice features

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -14,11 +14,13 @@ class PlaySoundsViewController: UIViewController {
     var player:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
     var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let url = receivedAudio.filePathUrl
         audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
         do{
             player = try AVAudioPlayer(contentsOfURL: url)
             player.enableRate = true
@@ -38,7 +40,28 @@ class PlaySoundsViewController: UIViewController {
         player.play()
     }
 
+    @IBAction func playVader(sender: UIButton) {
+        playAudioWithVariablePitch(-500)
+    }
+    
     @IBAction func playChipmunk(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        player.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        audioPlayerNode.play()
         
     }
     
